a2dfd5aebbeafa7b6df826ec650b309fba20672a
antonio <antoniojimdor@gmail.com>
2019-08-15 19:54:15 +0200

    19:54

--- luma/views.py
+++ luma/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Profesor
 # Create your views here.
 def listado (request):
-    return render(request, 'luma/listado.html', {})
+    profesores = Profesor.objects.all()
+    return render(request,'luma/listado.html',{'profesores': profesores})
